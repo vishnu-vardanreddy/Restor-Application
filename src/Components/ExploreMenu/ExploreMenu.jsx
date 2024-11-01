88f86e577efdfd7ac2016b6e1a637e2a501cb036
Vishnu <vishnuvardhanreddy1029@gmail.com>
2024-11-01 10:49:00 +0530

    Second Commit

---
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our Menu</h1>
+      <p className="explore-menu-text">
+        Modern restaurants offer everything from intimate farm-to-table
+        experiences to high-energy fusion kitchens, creating spaces where
+        traditional recipes meet innovative cooking techniques and where each
+        meal tells a unique story of flavors, culture, and creativity.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name,
+                )
+              }
+              key={index}
+              className="explore-menu-list-items"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt={item.menu_name}
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./Header.css";
+
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-content">
-        <div className="header-content">
-          <h2>Order your favourite food</h2>
-          <p>
-            Discover a culinary haven where every bite tells a story, from
-            sizzling grilled masterpieces to delicate desserts crafted with
-            passion, all served in an atmosphere that feels like a warm embrace
-            from an old friend.
-          </p>
-          <button>View-Menu </button>
-        </div>
+      <div
+        className="header-contents"
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          padding: "1.5vw",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        <h2>Order your favourite food</h2>
+        <p>
+          Discover a culinary haven where every bite tells a story, from
+          sizzling grilled masterpieces to delicate desserts crafted with
+          passion, all served in an atmosphere that feels like a warm embrace
+          from an old friend.
+        </p>
+        <button>View Menu</button>
       </div>
     </div>
   );
